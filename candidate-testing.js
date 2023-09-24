9bfc35181a172149438799ea91979d3dbac95490
stephenmdevine <stephenmdevine@gmail.com>
2023-09-24 00:19:15 -0500

    Part 2 completed with tests 1-10 passing

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,21 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,17 +35,27 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  console.log(question);
-  candidateAnswer = input.question(question);
+  // console.log(question);
+  // candidateAnswer = input.question(question);
+  for (let i = 0; i < questions.length; i++) {
+    let currentAnswer = input.question(questions[i]);
+    candidateAnswers.push(currentAnswer);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
-  console.log("That's correct. Good job!");
-} else {
-  console.log("The answer you provided is incorrect.");
-}
+// if (candidateAnswer == correctAnswer) {
+//   console.log("That's correct. Good job!");
+// } else {
+//   console.log("The answer you provided is incorrect.");
+// }
+console.log(`Correct Answer:\t\tCandidate Answer:
+${correctAnswers[0]}\t\t${candidateAnswers[0]}
+${correctAnswers[1]}\t\t\t${candidateAnswers[1]}
+${correctAnswers[2]}\t\t\t${candidateAnswers[2]}
+${correctAnswers[3]}\t\t${candidateAnswers[3]}
+${correctAnswers[4]}\t\t\t${candidateAnswers[4]}`);
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
